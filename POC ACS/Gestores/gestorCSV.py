a071933952e77a0a434e1ac3173095124aa20728
JavierCR16 <javier.contrerasm16@gmail.com>
2018-08-23 02:38:43 -0600

    Documentacion Doxygen, Falta agregar la de Keras

--- POC ACS/Gestores/gestorCSV.py
+++ POC ACS/Gestores/gestorCSV.py
@@ -3,9 +3,20 @@ Created on Aug 19, 2018
 
 @author: Javier
 '''
+
+## \package Gestores
+#  Contiene todas las funciones asociadas al manejo de archivos de extension CSV utilizando la libreria PANDAS
+#
+
 import pandas
 from collections import namedtuple
 
+
+## Descripcion de la funcion registroObjetos
+#
+#  Se encarga de agrupar los datos, que provienen en diccionarios, en tres categorias las cuales seran cargadas en un archivo CSV
+#  \param listaObjetos Es una lista de diccionarios y cada diccionario contiene los atributos de area, centroide e identificacion
+#  \return Nada
 def registroObjetos(listaObjetos):
     listaIdentificacion= [d["identificacion"] for d in listaObjetos ]
     listaCentroide= [d["centroide"] for d in listaObjetos]
--- POC ACS/Gestores/gestorImagenes.py
+++ POC ACS/Gestores/gestorImagenes.py
@@ -3,9 +3,19 @@ Created on Aug 19, 2018
 
 @author: Javier
 '''
+
+## \package Gestores
+#  Contiene todas las funciones asociadas al manejo de imagenes utilizando las librerias numpy y PIL
+#
 from PIL import Image
 import numpy as np
 
+## Descripcion de la funcion guardarImagen
+#
+#  Se encarga de guardar una imagen en el directorio de salida especificado
+#  \param nombreArchivo Es el nombre de la imagen a buscar en la carpeta static
+#  \param directorioArchivo Contiene el directorio de salida asi como el nombre de la imagen a guardar
+#  \return Nada
 def guardarImagen(nombreArchivo,directorioArchivo):
    imagenNueva= Image.fromarray(np.asarray(Image.open(nombreArchivo)))  
    imagenNueva.save(directorioArchivo)

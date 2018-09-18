087d62f5afb74e1211a4e01f72205f4d0edfb883
JavierCR16 <javier.contrerasm16@gmail.com>
2018-09-18 15:38:26 -0600

    Agregada ultima prueba unitaria

--- Avance #1 Proyecto ACS/PruebasUnitarias/PruebasUnitarias.py
+++ Avance #1 Proyecto ACS/PruebasUnitarias/PruebasUnitarias.py
@@ -14,23 +14,25 @@ class SimpleTestCase(unittest.TestCase):
     gestorImagenes = GestorImagenes()
     tuplaInformacionImagenes = gestorImagenes.obtenerImagenes(directorioImagenes,'../static/')
     
+    def test_directorioExistente(self):
+        self.assertTrue(isinstance(self.tuplaInformacionImagenes,tuple), "Directorio No Existe")
+    
     def test_archivosValidos(self):
-        self.assertTrue(isinstance(self.tuplaInformacionImagenes,tuple))
+        self.assertTrue(isinstance(self.tuplaInformacionImagenes,tuple), "Archivos no corresponden a un formato valido de Imagen")
         
     def test_valoresDevueltos(self):
         
-        self.assertTrue(isinstance(self.tuplaInformacionImagenes[0],list))
-        self.assertTrue(isinstance(self.tuplaInformacionImagenes[1],list))
+        self.assertTrue(isinstance(self.tuplaInformacionImagenes[0],list),"Valor no corresponde a una lista (1)")
+        self.assertTrue(isinstance(self.tuplaInformacionImagenes[1],list),"Valor no corresponde a una lista (2)")
         
     def test_imagenesProcesadas(self):
         cantidadImagenes = len([name for name in os.listdir(self.directorioImagenes)])
 
-        self.assertTrue(len(self.tuplaInformacionImagenes[0]) == cantidadImagenes)
-        self.assertTrue(len(self.tuplaInformacionImagenes[1]) == cantidadImagenes)
+        self.assertTrue(len(self.tuplaInformacionImagenes[0]) == cantidadImagenes,"La cantidad de imagenes del directorio seleccionado no corresponde con la cantidad de elementos de salida (1)")
+        self.assertTrue(len(self.tuplaInformacionImagenes[1]) == cantidadImagenes,"La cantidad de imagenes del directorio seleccionado no corresponde con la cantidad de elementos de salida (2)")
         
-   
+    
         
-
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
